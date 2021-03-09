43b5b47824f22b174cf91adc2f468ab793afa144
Crionous <44718166+Crionous@users.noreply.github.com>
2021-03-09 18:14:32 +0700

    pet system in progress
    baru sabi sampe ngescroll petnya pas di pet selection scene, sm ada beberapa kodingan jg yg udh siap bwt di jalanin
    
    cmn yg msh ngebug itu di bagian save/load datanya bwt ngecek apakah pet sudah equip atau belum

---
+++ Assets/Scripts/PetInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "PetInfo")]
+[System.Serializable]
+public class PetInfo : ScriptableObject
+{
+    public string petName;
+    [TextArea(3, 1)] public string petDescription;
+    public StatusType statusType;
+    public float statusValue;
+    public Sprite spriteSource;
+    public bool isActive;
+    public bool purchaseStatus;
+}
+
+public enum StatusType
+{
+    PowerUp,
+    SpeedUp
+}
--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -4,6 +4,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
+    //save data
     public static void SaveData(GameManager manager)
     {
         BinaryFormatter formatter = new BinaryFormatter();
@@ -28,7 +29,20 @@ public static class SaveSystem
         formatter.Serialize(stream, data);
         stream.Close();
     }
+    public static void SavePetManager(PetManager pManager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
 
+        string path = "D:/SaveFile/petData.uwansummoney";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData data = new GameData(pManager);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    //load data
     public static GameData LoadData ()
     {
         string path = "D:/SaveFile/data.uwansummoney";
@@ -51,6 +65,25 @@ public static class SaveSystem
     public static GameData LoadQuestState()
     {
         string path = "D:/SaveFile/questData.uwansummoney";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("save file not found in " + path);
+            return null;
+        }
+    }
+    public static GameData LoadPetManager()
+    {
+        string path = "D:/SaveFile/petData.uwansummoney";
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
