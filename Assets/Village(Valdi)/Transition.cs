d693c19298968e78d61de197f9b30fb5c721093c
valdi-ss <79294864+valdi-ss@users.noreply.github.com>
2021-03-10 23:20:13 +0700

    transition,Village,ShopUI
    Masih ada Beberapa yang kurang dari Artist blom ada di Drive

---
+++ Assets/Village(Valdi)/Transition.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+
+    public Animator transition;
+    public float time = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Merchant"))
+        {
+           // LoadNextLevel();
+            SceneManager.LoadScene("Shop(UI)");
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Village"))
+        {
+            SceneManager.LoadScene("Village");
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Shop(UI)");
+        //StartCoroutine(LoadLevel("Shop(UI)"));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene(levelIndex);
+
+    }
+}
