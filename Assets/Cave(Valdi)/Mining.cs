f0d2f73e5759c86c6fbfcfe8d10f537861f9f504
valdi-ss <79294864+valdi-ss@users.noreply.github.com>
2021-04-12 16:08:20 +0700

    cave

---
+++ Assets/Cave(Valdi)/Mining.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Mining : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Mine"))
+        {
+            anim.SetBool("Mine", true);
+        }
+        else
+        {
+            anim.SetBool("Mine", false);
+        }
+    }
+}
