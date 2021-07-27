71b2306ef49fd171fa655b3cbc9e55914dd05cc6
JEKA-2 <79297604+JEKA-2@users.noreply.github.com>
2021-07-28 02:44:28 +0700

    Ads
    ads

---
+++ Assets/Scripts/Plugin/AdsManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+
+public class AdsManager : MonoBehaviour, IUnityAdsListener
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize("4101371");
+        Advertisement.AddListener(this);
+    }
+
+    public void PlayAd()
+    {
+        if (Advertisement.IsReady("GetMoneh"))
+        {
+            Advertisement.Show("GetMoneh");
+        }
+    }
+
+    public void PlayRewardedAd()
+    {
+        if (Advertisement.IsReady("AddMoneh"))
+        {
+            Advertisement.Show("AddMoneh");
+        }
+        else
+        {
+            Debug.Log("Ads Is Not Ready!");
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        Debug.Log("Ads Are Ready");
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.Log("Error" + message);
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        Debug.Log("Video started");
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (placementId == "AddMoneh" && showResult == ShowResult.Finished)
+        {
+            Debug.Log("Player Rewarded");
+        }
+    }
+}
